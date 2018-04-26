cd0ccff1d9234dfd12d13e8966ac376948271413
Athena <athena@Chi-Cheongs-MacBook-Pro.local>
2018-04-26 10:16:14 +0800

    init check-in

---
+++ royalty_management_system/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for royalty_management_system project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "%s.settings" % real_project_name("royalty_management_system"))
+
+application = get_wsgi_application()
