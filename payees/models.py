e08fc4de062aa9928bd11c1d9f4aa0a0d405841a
Chi-Cheong Weng <chicheongweng@gmail.com>
2018-04-26 13:33:02 +0800

    updated Payee fields

--- payees/models.py
+++ payees/models.py
@@ -8,10 +8,10 @@ from channels.models import Channel
 from royalty.models import Royalty
 
 class Payee(models.Model):
-    TYPE = Choices(_('Company'), _('Person'))
+    TYPE = Choices(_('Company'), _('Singer'), _('Author'))
     name = models.CharField(_('name'), max_length=256)
     royalty = models.ForeignKey(Royalty, on_delete=models.CASCADE)
-    amount = models.FloatField(_('Amount'), default=0.0)
+    percentage = models.FloatField(_('percentage'), default=0.0)
 
     class Meta:
         verbose_name = _('Payee')
